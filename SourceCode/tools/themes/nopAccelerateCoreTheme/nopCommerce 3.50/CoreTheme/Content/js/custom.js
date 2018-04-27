84c3daf13f9f482c8657e28c280c1cb0d6fdb0ad
Quang Hoa <quanghoahcm@gmail.com>
2018-04-27 13:19:58 +0700

    Init
    Init vi-languae and CAPtCHA

---
+++ SourceCode/tools/themes/nopAccelerateCoreTheme/nopCommerce 3.50/CoreTheme/Content/js/custom.js
@@ -0,0 +1,23 @@
+﻿/*Custom js for stick header menu*/
+//http://www.jqueryscript.net/menu/Sticky-Navigation-Bar-with-jQuery-Bootstrap.html
+
+$(document).ready(function () {
+    $(window).bind('scroll', function() {
+        //var navHeight = $("nav.top-menu").height();
+        var navHeight = $("div.header").height();
+        var navWidth = $("div.header").width();
+        ($(window).scrollTop() > navHeight) ? $('.navbar.navbar-inverse').addClass('goToTop').width(navWidth) : $('.navbar.navbar-inverse').removeClass('goToTop');
+    });
+});
+
+$(window).resize(function () {    
+    $(".navbar.navbar-inverse").width("100%");
+    $(window).scrollTop(1);
+});
+$(document).ready(function () {
+    $('#exit ').click(function (e) {
+        $('.responsive').hide();
+        $('.master-wrapper-page').css('margin-top', '0');
+        $('.header-links').css('margin-top', '20px');
+    });
+});
