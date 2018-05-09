12c22d926128264614750ca6f28ada8709d36841
Quang Hoa <quanghoahcm@gmail.com>
2018-05-09 15:19:50 +0700

    Revert "11"xc
    This reverts commit 6fbb1b2019a32a7d03648db7d6f6f7466cad9f58.

---
+++ SourceCode/nopCommerce_4.00_Source/Plugins/Nop.Plugin.Api/Infrastructure/DependencyRegister.cs
@@ -0,0 +1,90 @@
+﻿using Autofac;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+using Nop.Plugin.Api.Services;
+
+namespace Nop.Plugin.Api.Infrastructure
+{
+    using System;
+    using Nop.Core.Domain.Catalog;
+    using Nop.Core.Domain.Common;
+    using Nop.Core.Domain.Customers;
+    using Nop.Core.Domain.Orders;
+    using Nop.Plugin.Api.Converters;
+    using Nop.Plugin.Api.Data;
+    using Nop.Plugin.Api.Factories;
+    using Nop.Plugin.Api.Helpers;
+    using Nop.Plugin.Api.JSON.Serializers;
+    using Nop.Plugin.Api.ModelBinders;
+    using Nop.Plugin.Api.Validators;
+    using Nop.Plugin.Api.WebHooks;
+    using Nop.Web.Framework.Infrastructure;
+
+    public class DependencyRegister : IDependencyRegistrar
+    {
+        private const string ObjectContextName = "nop_object_context_web_api";
+
+        public void Register(ContainerBuilder builder, ITypeFinder typeFinder, NopConfig config)
+        {
+            this.RegisterPluginDataContext<ApiObjectContext>(builder, ObjectContextName);
+
+            RegisterPluginServices(builder);
+
+            RegisterModelBinders(builder);
+        }
+
+        private void RegisterModelBinders(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(ParametersModelBinder<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(JsonModelBinder<>)).InstancePerLifetimeScope();
+        }
+
+        private void RegisterPluginServices(ContainerBuilder builder)
+        {
+            builder.RegisterType<ClientService>().As<IClientService>().InstancePerLifetimeScope();
+            builder.RegisterType<CustomerApiService>().As<ICustomerApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<CategoryApiService>().As<ICategoryApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<ProductApiService>().As<IProductApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<ProductCategoryMappingsApiService>().As<IProductCategoryMappingsApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<OrderApiService>().As<IOrderApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<ShoppingCartItemApiService>().As<IShoppingCartItemApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<OrderItemApiService>().As<IOrderItemApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<ProductAttributesApiService>().As<IProductAttributesApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<ProductPictureService>().As<IProductPictureService>().InstancePerLifetimeScope();
+            builder.RegisterType<ProductAttributeConverter>().As<IProductAttributeConverter>().InstancePerLifetimeScope();
+            builder.RegisterType<NewsLetterSubscriptionApiService>().As<INewsLetterSubscriptionApiService>().InstancePerLifetimeScope();
+
+            builder.RegisterType<MappingHelper>().As<IMappingHelper>().InstancePerLifetimeScope();
+            builder.RegisterType<CustomerRolesHelper>().As<ICustomerRolesHelper>().InstancePerLifetimeScope();
+            builder.RegisterType<JsonHelper>().As<IJsonHelper>().InstancePerLifetimeScope();
+            builder.RegisterType<DTOHelper>().As<IDTOHelper>().InstancePerLifetimeScope();
+            builder.RegisterType<NopConfigManagerHelper>().As<IConfigManagerHelper>().InstancePerLifetimeScope();
+
+            builder.RegisterType<NopWebHooksLogger>().As<Microsoft.AspNet.WebHooks.Diagnostics.ILogger>().InstancePerLifetimeScope();
+
+            builder.RegisterType<JsonFieldsSerializer>().As<IJsonFieldsSerializer>().InstancePerLifetimeScope();
+
+            builder.RegisterType<FieldsValidator>().As<IFieldsValidator>().InstancePerLifetimeScope();
+
+            builder.RegisterType<WebHookService>().As<IWebHookService>().SingleInstance();
+
+            builder.RegisterType<ObjectConverter>().As<IObjectConverter>().InstancePerLifetimeScope();
+            builder.RegisterType<ApiTypeConverter>().As<IApiTypeConverter>().InstancePerLifetimeScope();
+
+            builder.RegisterType<CategoryFactory>().As<IFactory<Category>>().InstancePerLifetimeScope();
+            builder.RegisterType<ProductFactory>().As<IFactory<Product>>().InstancePerLifetimeScope();
+            builder.RegisterType<CustomerFactory>().As<IFactory<Customer>>().InstancePerLifetimeScope();
+            builder.RegisterType<AddressFactory>().As<IFactory<Address>>().InstancePerLifetimeScope();
+            builder.RegisterType<OrderFactory>().As<IFactory<Order>>().InstancePerLifetimeScope();
+            builder.RegisterType<ShoppingCartItemFactory>().As<IFactory<ShoppingCartItem>>().InstancePerLifetimeScope();
+
+            builder.RegisterType<Maps.JsonPropertyMapper>().As<Maps.IJsonPropertyMapper>().InstancePerLifetimeScope();
+        }
+
+        public virtual int Order
+        {
+            get { return Int16.MaxValue; }
+        }
+    }
+}
